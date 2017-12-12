2058f5a02da5d25097e7429600c0775b1e982c91
Lera Kruchek <lera.kruchek@gmail.com>
2017-12-12 21:46:24 +0300

    npm scripts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'git@github.com:LeraKruchek/deploy.git',
       path: '/home/ubuntu/deploy',
-      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      'post-deploy': 'npm install && npm run restart'
     }
   }
 };
