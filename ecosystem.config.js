5f6e0110e91d2e6fe7d49e0c6dfbeffca9cb2612
Lera Kruchek <lera.kruchek@gmail.com>
2017-12-12 20:22:01 +0300

    pm2 deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'deploy',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-217-247-15.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/hackermoon_nodejs_key.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:LeraKruchek/deploy.git',
+      path: '/home/ubuntu/deploy',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+};
